a22ad8ac8ef696b6effb92137e3909b31a98f0f8
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2025-01-01 20:39:51 +1300

    Failed attempt to use web workers

--- src/GameboyColorReducer.Wasm/Program.cs
+++ src/GameboyColorReducer.Wasm/Program.cs
@@ -1,3 +1,4 @@
+using BlazorWorker.Core;
 using GameboyColorReducer.Wasm;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -5,6 +6,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddWorkerFactory();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
